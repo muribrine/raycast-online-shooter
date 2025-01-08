50b43ded2a855bc72c2279b41152492372e46cd8
Muribrine <muribrine@gmail.com>
2025-01-08 20:43:55 -0300

    Starting a new with golang.

--- frontend/js/audio.js
+++
@@ -1,8 +0,0 @@
-function create_audio_handler(source, volume) {
-    const HANDLER = document.createElement('audio');
-    HANDLER.id = `${source}`;
-    HANDLER.src = `${source}`;
-    HANDLER.volume = volume;
-    document.body.appendChild(HANDLER);
-    return HANDLER;
-}
---
+++ raycast-online-shooter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	const addr string = ":8080"
+
+	g := gin.Default()
+
+	g.LoadHTMLFiles("./public/index.html")
+
+	g.GET("/", func(ctx *gin.Context) { ctx.HTML(http.StatusOK, "index.html", gin.H{}) })
+	g.GET("/style.css", func(ctx *gin.Context) { ctx.File("./public/style.css") })
+	g.GET("/core.js", func(ctx *gin.Context) { ctx.File("./public/core.js") })
+
+	fmt.Println("Listening on localhost" + addr)
+	g.Run(addr)
+
+}
